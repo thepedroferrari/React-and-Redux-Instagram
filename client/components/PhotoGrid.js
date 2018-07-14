af265babf2d4e51a67e92a790679641d32663c2d
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-14 13:12:51 +0200

    Startup with React Router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+	render() {
+		return <div className="photo-grid">I'm the Photo Grid!</div>;
+	}
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Single = React.createClass({
+	render() {
+		return <div className="single-photo">I'm the Single</div>;
+	}
+});
+
+export default Single;
