45d25be0cf16003a471e067c4c099cf0a4353a32
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-15 01:25:50 +0200

    Displaying Redux inside components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
 	render() {
-		return <div className="photo-grid">I'm the Photo Grid!</div>;
+		return (
+			<div className="photo-grid">
+				{this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+			</div>
+		);
 	}
 });
 
