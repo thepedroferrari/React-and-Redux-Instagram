4f70302b0fc0128168e3fa934ee2f5d5ee8f2378
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-15 03:07:18 +0200

    Adding the single photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
 	render() {
-		return <div className="single-photo">I'm the Single</div>;
+		// index of the post
+		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[i];
+		// get us the post
+		return (
+			<div className="single-photo">
+				<Photo i={i} post={post} {...this.props} />
+				<Comments />
+			</div>
+		);
 	}
 });
 
